5ce9192e135e78b9bbc3b1e86a75df5aa66e9e39
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-07-31 21:56:47 +0545

    sidebar design

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./sidebar.css";
 import AddCircleSharpIcon from '@mui/icons-material/AddCircleSharp';
 export default function Sidebar() {
+  const colors =["red","blue","lightgreen","yellow","cyan"]
+  const [listOpen,setlistOpen]=useState(false)
   return (
-    <div className="plus_icon">
-      <AddCircleSharpIcon sx={{ fontSize: 80 }}/>
-      <ul>
-        <li>Green</li>
-        <li>Cyan</li>
-        <li>Pink</li>
-        <li>Yellow</li>
-        <li>Blue</li>
-      </ul>
+    <div className="sideBar">
+      <AddCircleSharpIcon sx={{ fontSize: 50 }} className="plus" onClick={()=>setlistOpen(!listOpen)} />
+        <ul className={`sideBar-list ${listOpen?"sideBar-list-active":""}`}>
+          {
+            colors.map((item,index)=><li key={index} className="sideBar-list-items" style={{backgroundColor:item}}/>)
+          }
+          
+          
+        </ul>
     </div>
 
   )
